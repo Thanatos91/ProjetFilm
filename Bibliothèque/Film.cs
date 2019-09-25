d16eb1da6a4e6c7bdba1cec8302bd98abe4844d2
Thanatos91 <fhachemi@lerebours.org>
2019-09-25 15:44:46 +0200

    XAML presque fini, manque à alimenter le GridView, c'est chaud

--- Bibliothèque/Film.cs
+++ Bibliothèque/Film.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bibliothèque
 {
@@ -7,5 +8,12 @@ namespace Bibliothèque
         public string TitreFilm { get; set; }
         public int NbEntrees { get; set; }
         public string PhotoFilm { get; set; }
+        public Realisateur LeRealisateur { get; set; }
+        public List<Acteur> LesActeurs { get; set; }
+
+        public void AjouterActeur(Acteur unActeur)
+        {
+            LesActeurs.Add(unActeur);
+        }
     }
 }
